aac847c9da71f3948a4a84e4f93e1140684791c9
Trud-Hong <hws7146@naver.com>
2026-01-31 21:59:27 +0900

    feat:질문 수정 기능 추가

--- src/main/java/com/mysite/sbb/answer/Answer.java
+++ src/main/java/com/mysite/sbb/answer/Answer.java
@@ -13,7 +13,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -37,5 +36,7 @@ public class Answer {
 
   @ManyToOne
   private SiteUser author;
+
+  private LocalDateTime modifyDate;
 
 }
